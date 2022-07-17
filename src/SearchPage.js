e000eafcd0500101ebd5a2f478e09deb4c0b0dff
prateek800 <prateek2010092@akgec.ac.in>
2022-07-18 02:27:11 +0530

    Search page updated

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,21 +1,78 @@
-import React from 'react'
-import './SearchPage.css'
-import { useStateValue } from './StateProvider'
+import React from "react";
+import "./SearchPage.css";
+import { useStateValue } from "./StateProvider";
+import useGoogleSearch from "./useGoogleSearch";
+import Response from "./response";
+import { Link } from "react-router-dom";
+import Search from "./Search";
+import SearchIcon from "@mui/icons-material/Search";
+import DescriptionIcon from "@mui/icons-material/Description";
+import ImageIcon from "@mui/icons-material/Image";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+import RoomIcon from "@mui/icons-material/Room";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
 
 const SearchPage = () => {
-
   const [{ term }, dispatch] = useStateValue();
+  //   const { data } = useGoogleSearch(term);
 
+  const data = Response;
+
+  console.log(data);
   return (
-    <div>
-        <div className='searchPage_header'>
-            <h1>{term}</h1>
-        </div>
-        <div className='searchPage_results'>
+    <div className="searchPage">
+      <div className="searchPage_header">
+        <Link to="/">
+          <img
+            className="searchPage_logo"
+            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+            alt=""
+          />
+        </Link>
 
+        <div className="searchPage_headerBody">
+          <Search hideButtons />
+
+          <div className="searchPage_options">
+            <div className="searchPage_optionsLeft">
+              <div className="searchPage_option">
+                <SearchIcon />
+                <Link to="/all">All</Link>
+              </div>
+              <div className="searchPage_option">
+                <DescriptionIcon />
+                <Link to="/news">News</Link>
+              </div>
+              <div className="searchPage_option">
+                <ImageIcon />
+                <Link to="/images">Images</Link>
+              </div>
+              <div className="searchPage_option">
+                <LocalOfferIcon />
+                <Link to="/shopping">shopping</Link>
+              </div>
+              <div className="searchPage_option">
+                <RoomIcon />
+                <Link to="/maps">maps</Link>
+              </div>
+              <div className="searchPage_option">
+                <MoreVertIcon />
+                <Link to="/more">more</Link>
+              </div>
+              
+            </div>
+
+            <div className="searchPage_optionsRight">
+
+            </div>
+          </div>
         </div>
+      </div>
+
+      <div className="searchPage_results"></div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchPage
+export default SearchPage;
